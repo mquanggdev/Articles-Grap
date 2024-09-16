9be25cc6fdc390b4c0af5babeb300416649055fd
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-16 10:50:16 +0700

    3 : Cài đặt và sử dụng graphql

--- index.ts
+++ index.ts
@@ -1,23 +1,30 @@
 import express, { Express, Request, Response } from "express";
 import env from "dotenv";
-env.config();
+import { ApolloServer } from "apollo-server-express";
 import { connect } from "./config/database";
-import Article from "./models/articles.model";
+import { gql } from "apollo-server-express";
+import { typeDefs } from "./typedefs";
+import { resolvers } from "./resolvers";
 
-connect();
-const app: Express = express();
-const port: (number | string) = `${process.env.PORT}` || 3000;
 
-// Rest API
-app.get("/articles",  async (req: Request, res: Response) => {
-    const articles = await Article.find({
-        deleted: false
-      });
-  res.json({
-    articles: articles
-  });
-});
+const startServer = async () => {
+    env.config();
+    connect();
+    const app: Express = express();
+    const port: (number | string) = `${process.env.PORT}` || 3000;
+    //Graphql
+    const apolloServer = new ApolloServer({
+        typeDefs: typeDefs, // thằng này định nghĩa kiểu dữ liệu
+        resolvers: resolvers // thằng này thì trả dữ liệu dựa trên key được định nghĩa ở typeDefs
+    })
+    await apolloServer.start();
+    apolloServer.applyMiddleware({
+        app : app,
+        path : `/graphql`
+    })
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+    app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    });
+}
+startServer();
