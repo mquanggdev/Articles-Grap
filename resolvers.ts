16eef9fe029b131247fd86d5b2ca17cdb3645da2
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-16 23:10:47 +0700

    10 : Quản lý danh mục

--- resolvers.ts
+++ resolvers.ts
@@ -1,4 +1,5 @@
 import Article from "./models/articles.model";
+import Category from "./models/category.model";
 
 export const resolvers = {
     Query: {
@@ -15,7 +16,24 @@ export const resolvers = {
           deleted : false
         });
         return article ;
-      }
+      },
+      getListCategory: async () => {
+        const categories = await Category.find({
+          deleted: false
+        });
+  
+        return categories;
+      },
+      getCategory: async (_, args) => {
+        const { id } = args;
+  
+        const category = await Category.findOne({
+          _id: id,
+          deleted: false
+        });
+  
+        return category;
+      },
     } ,
     Mutation: {
       createArticle : async (_ , args) => {
@@ -43,6 +61,40 @@ export const resolvers = {
         }, article);
   
         const data = await Article.findOne({
+          _id: id,
+          deleted: false
+        });
+  
+        return data;
+      },createCategory: async (_, args) => {
+        const { category } = args;
+  
+        const newCategory = new Category(category);
+        await newCategory.save();
+  
+        return newCategory;
+      },
+      deleteCategory: async (_, args) => {
+        const { id } = args;
+  
+        await Category.updateOne({
+          _id: id
+        }, {
+          deleted: true,
+          deletedAt: new Date()
+        });
+  
+        return "Đã xóa";
+      },
+      updateCategory: async (_, args) => {
+        const { id, category } = args;
+  
+        await Category.updateOne({
+          _id: id,
+          deleted: false
+        }, category);
+  
+        const data = await Category.findOne({
           _id: id,
           deleted: false
         });
--- typeDefs.ts
+++ typeDefs.ts
@@ -6,12 +6,19 @@ export const typeDefs = gql`
     title : String , 
     avatar : String , 
     description : String , 
+  }
 
+  type Category {
+    id: String,
+    title: String,
+    avatar: String
   }
   
   type Query { 
     getListArticles: [Article],
-    getArticle(id:ID) : Article
+    getArticle(id:ID) : Article,
+    getListCategory: [Category],
+    getCategory(id: ID): Category,
   }
 
   input ArticleInput {
@@ -19,9 +26,17 @@ export const typeDefs = gql`
     avatar : String ,
     description : String 
   }
+  input CategoryInput {
+    title: String,
+    avatar: String
+  }
+
   type Mutation {
     createArticle(article:ArticleInput):Article,
     deleteArticle(id: String): String,
     updateArticle(id: String, article: ArticleInput): Article,
+    createCategory(category: CategoryInput): Category,
+    deleteCategory(id: String): String,
+    updateCategory(id: String, category: CategoryInput): Category,
   }
 `
