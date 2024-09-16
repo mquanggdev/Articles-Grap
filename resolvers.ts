dab5f52781eef5e54c817b3f579934577b74e9b2
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-16 22:15:32 +0700

    4 : CRUD : Lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,12 @@
+import Article from "./models/articles.model";
+
 export const resolvers = {
     Query: {
-      hello: () => {
-        return "Hello World!";
+      getListArticles : async () => {
+        const articles = await Article.find({
+          deleted : false
+        });
+        return articles ;
       }
     }
   }
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,7 +1,14 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+  type Articles {
+    id : ID,
+    title : String , 
+    avatar : String , 
+    description : String , 
+
+  }
   type Query {
-    hello: String
+    getListArticles : [Articles]
   }
 `
